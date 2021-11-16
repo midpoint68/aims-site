632e14a916cef22764a2e433b85d1a46b8b02f62
midpoint68 <trmidpoint@gmail.com>
2021-11-16 14:31:52 -0500

    move base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -10,6 +12,9 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+		paths: {
+			base: dev ? '' : '/aims-site',
+		},
 		adapter: adapter({
 			// default options are shown
 			pages: 'docs',
